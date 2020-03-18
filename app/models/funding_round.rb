882c9e9175e9902722d0b1b9881b0b306a773fad
AidanMcB <aidankmcbride@gmail.com>
2020-03-18 16:17:53 -0500

    Finished pairing project as of March 18th

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,20 @@
 class FundingRound
+    @@all = []
+    attr_accessor :type, :investment
+    attr_reader :startup, :venture_capitalist 
+
+    def initialize(options)
+        @startup = options[:startup]
+        @venture_capitalist = options[:venture_capitalist]
+        @type = options[:type]
+        @investment = options[:investment]
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all 
+    end
+
+    
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,7 +11,15 @@ end
 
 star1 = Startup.new({name:"Rafa's pizzas",founder:"JPMorgan",domain:"Rafaspizzas.com"})
 star2 = Startup.new({name:"Alma's beers",founder:"Wellfargo",domain:"Almasbeers.com"})
+star3 = Startup.new({name:"Aidan's Burgers",founder:"Chase",domain:"Aidansburger.com"})
 
+ven1 = VentureCapitalist.new({name:"RickyRicon",total_worth: 100000})
+ven2 = VentureCapitalist.new({name:"John Rockafeller", total_worth: 2000000000})
+ven3 = VentureCapitalist.new({name:"William the Wild", total_worth: 345630000000})
 
+fund1 = FundingRound.new({startup: star1, venture_capitalist: ven1, type: "Series A", investment: 500.00})
+fund2 = FundingRound.new({startup: star2, venture_capitalist: ven2, type: "Series B", investment: 600.00})
+fund3 = FundingRound.new({startup: star1, venture_capitalist: ven2, type: "Series c", investment: 99.00})
+fund4 = FundingRound.new({startup: star1, venture_capitalist: ven1, type: "Series A", investment: 9898.00})
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
